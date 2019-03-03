1540bb3816392b0c34adb0f0d05e170ecb58e615
Mingchao Zhang <mz5@illinois.edu>
2019-03-03 03:06:02 -0600

    set up DATABASES in settings.py

--- food_fax/food_fax/settings.py
+++ food_fax/food_fax/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'sample.apps.SampleConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ food_fax/sample/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Sample(models.Model):
+    Food_ID = models.CharField(max_length=200)
+    UPC = models.CharField(max_length=200)
+    Product_Name = models.CharField(max_length=200)
+    Food_Group = models.CharField(max_length=200)
+    Calories = models.DecimalField(max_digits=10000, decimal_places=5)
+    Total_Carbohydrates = models.DecimalField(max_digits=10000, decimal_places=5)
+    Protein = models.DecimalField(max_digits=10000, decimal_places=5)
+    Total_Fat = models.DecimalField(max_digits=10000, decimal_places=5)
+    Sodium = models.DecimalField(max_digits=10000, decimal_places=5)
+    Cholesterol = models.DecimalField(max_digits=10000, decimal_places=5)
+    Sugars = models.DecimalField(max_digits=10000, decimal_places=5)
+    # main field / key
+    def __str__(self):
+        return self.Food_ID
+
