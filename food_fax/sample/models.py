0f7004f5d6070abf974290468590967556fba390
Mingchao Zhang <mz5@illinois.edu>
2019-03-03 03:12:42 -0600

    migrate sample table

--- food_fax/sample/models.py
+++ food_fax/sample/models.py
@@ -5,13 +5,13 @@ class Sample(models.Model):
     UPC = models.CharField(max_length=200)
     Product_Name = models.CharField(max_length=200)
     Food_Group = models.CharField(max_length=200)
-    Calories = models.DecimalField(max_digits=10000, decimal_places=5)
-    Total_Carbohydrates = models.DecimalField(max_digits=10000, decimal_places=5)
-    Protein = models.DecimalField(max_digits=10000, decimal_places=5)
-    Total_Fat = models.DecimalField(max_digits=10000, decimal_places=5)
-    Sodium = models.DecimalField(max_digits=10000, decimal_places=5)
-    Cholesterol = models.DecimalField(max_digits=10000, decimal_places=5)
-    Sugars = models.DecimalField(max_digits=10000, decimal_places=5)
+    Calories = models.DecimalField(max_digits=20, decimal_places=5)
+    Total_Carbohydrates = models.DecimalField(max_digits=20, decimal_places=5)
+    Protein = models.DecimalField(max_digits=20, decimal_places=5)
+    Total_Fat = models.DecimalField(max_digits=20, decimal_places=5)
+    Sodium = models.DecimalField(max_digits=20, decimal_places=5)
+    Cholesterol = models.DecimalField(max_digits=20, decimal_places=5)
+    Sugars = models.DecimalField(max_digits=20, decimal_places=5)
     # main field / key
     def __str__(self):
         return self.Food_ID
