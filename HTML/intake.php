37cc5189f7f5c31336fa96f0a266a4f143c2e56e
stevegonzo <stevejgonzo@gmail.com>
2019-04-01 01:11:47 -0500

    added intake.php

--- HTML/intake.php
+++ HTML/intake.php
@@ -66,8 +66,8 @@
   </head>
     <body>
             <div class="container">
-                    <header class="blog-header py-3">
-                      <div class="row flex-nowrap justify-content-between align-items-center">
+                <header class="blog-header py-3">
+                    <div class="row flex-nowrap justify-content-between align-items-center">
                         <div class="col-4 pt-1">
                           <span class="text-primary">Hello, <?php echo $name;?>!</span>
                         </div>
@@ -77,85 +77,27 @@
                         <div class="col-4 d-flex justify-content-end align-items-center">
                             <a class="text-muted" href="contact.html">Contact</a>
                         </div>
-                      </div>
-                    </header>
+                    </div>
+                </header>
                   
-                    <div class="nav-scroller py-1 mb-2">
-                      <nav class="nav d-flex justify-content-between">
-                        <a class="p-2 text-muted" href="intake.php">Nutrient Intake</a>
+                <div class="nav-scroller py-1 mb-2">
+                    <nav class="nav d-flex justify-content-between">
+                        <a class="p-2 text-muted" href="#">Nutrient Profile</a>
                         <a class="p-2 text-muted" href="#">Nutrient Targets</a>
                         <a class="p-2 text-muted" href="#">Recipes</a>
                         <a class="p-2 text-muted" href="#">Food Items</a>
-                      </nav>
-                    </div>
+                    </nav>
+                </div>
                   
-                    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-                      <div class="col-md-6 px-0">
+                <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+                    <div class="col-md-6 px-0">
                         <h1 class="display-4 font-italic">Your Profile</h1>
-                      </div>
                     </div>
+                </div>
                   
-                    <div class="row mb-2">
-                      <div class="col-md-6">
-                        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                          <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-primary">Macro and Micro Nutrient Profile</strong>
-                            <h3 class="mb-0">View your nutrient intake!</h3>
-                            <div class="mb-1 text-muted">As of Feb 12</div>
-                            <p class="card-text mb-auto">A history of your nutrient intake for the last 10 days.</p>
-                            <a href="intake.php" class="stretched-link">View Intake</a>
-                          </div>
-                          <div class="col-auto d-none d-lg-block">
-                            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                          <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-primary">Your Nutrient Targets</strong>
-                            <h3 class="mb-0">See your current nutrient targets!</h3>
-                            <div class="mb-1 text-muted">Macro and Micro Nutrients</div>
-                            <p class="mb-auto">As determined by your profile and FDA recommendations.</p>
-                            <a href="#" class="stretched-link">View Targets</a>
-                          </div>
-                          <div class="col-auto d-none d-lg-block">
-                            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row mb-2">
-                            <div class="col-md-6">
-                              <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                <div class="col p-4 d-flex flex-column position-static">
-                                  <strong class="d-inline-block mb-2 text-success">Recipes</strong>
-                                  <h3 class="mb-0">Your recommended recipes!</h3>
-                                  <div class="mb-1 text-muted">Today</div>
-                                  <p class="card-text mb-auto">Delicious recipes to help you reach your targets.</p>
-                                  <a href="#" class="stretched-link">View Recipes</a>
-                                </div>
-                                <div class="col-auto d-none d-lg-block">
-                                  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                </div>
-                              </div>
-                            </div>
-                            <div class="col-md-6">
-                              <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                                <div class="col p-4 d-flex flex-column position-static">
-                                  <strong class="d-inline-block mb-2 text-success">Food Items</strong>
-                                  <h3 class="mb-0">Your recommended raw foods!</h3>
-                                  <div class="mb-1 text-muted">Today</div>
-                                  <p class="mb-auto">Individual food items to help you reach your targets.</p>
-                                  <a href="#" class="stretched-link">View Raw Foods</a>
-                                </div>
-                                <div class="col-auto d-none d-lg-block">
-                                  <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                  </div>
+                    
+                      
+                    
                   
                   <main role="main" class="container">
                     <div class="row">
