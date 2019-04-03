3a280f5e7fc634b089a66e1e4996e784a5ccecfd
Rohan Prabhakar <36459840+rohan-bp@users.noreply.github.com>
2019-04-03 15:04:46 -0500

    Added recipes script

--- Workspace/recipe_script.py
+++ Workspace/recipe_script.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+import os
 
 """
     All recipes use the following units:
@@ -9,30 +9,75 @@ import json
 """
 
 dataset_file = "epicurious-recipes-with-rating-and-nutrition/full_format_recipes.json"
-results = "recipes.csv"
+recipes_file = "recipes.csv" # recip_id,name,description,directions
+ingredients_file = "ingredients.csv" # recip_id,ingredient_name,quantity
+input_file = "input.txt"
+output_file = "results.json"
+command = "python bin/parse-ingredients.py input.txt > results.txt && python bin/convert-to-json.py results.txt > results.json"
+delimiter = "~"
+recipe_id_offset = 5000000
 
-ingredients = {}
-csv_output = {}
+ingredients_output = []
+recipe_output = {}
 
 with open(dataset_file) as file:
     data = json.load(file)
     count = 0
+    os.chdir("ingredient-phrase-tagger-master/")
     for each_recipe in data:
+        if(count > 10):
+            break
         try:
-            key = each_recipe["title"]
-            ingredients[key] = each_recipe["ingredients"]
-            csv_output[key] = [key,str(each_recipe["directions"][0])]
+            recipe_title = each_recipe["title"].strip()
+            key = count + recipe_id_offset
             
+            file_content = "\n".join(each_recipe["ingredients"])
+            
+            with open(input_file, "w") as f:
+                f.write(file_content)
+            
+            os.system(command)
+            
+            with open(output_file) as f:
+                ingredient_data = json.load(f)
+            
+                for each_ingredient in ingredient_data:
+                    name = each_ingredient["name"]
+                    unit = ""
+                    quantity = ""
+                    
+                    if('unit' in each_ingredient.keys()):
+                        unit = each_ingredient["unit"]
+                    if('qty' in each_ingredient.keys()):
+                        quantity = each_ingredient["qty"]
+                    if('qty' not in each_ingredient.keys()):
+                        try:
+                            quantity = float(each_ingredient["other"])
+                        except:
+                            continue
+
+                    line = [str(key),str(name),str(quantity),str(unit)]
+                    print(line)
+                    ingredients_output.append(line)
+
+            recipe_output[key] = [str(key),str(recipe_title)]
             count += 1
-        
-        except:
+                
+        except Exception as e:
+            print(e)
             continue
 
     print(count)
+    os.chdir("../")
 
-with open(results, "w") as output:
-    writer = csv.writer(output, delimiter ='~',lineterminator='\n')
-    for each_recipe in csv_output.keys():
-        line = csv_output[each_recipe]
+with open(recipes_file, "w") as output:
+    writer = csv.writer(output, delimiter = delimiter,lineterminator='\n')
+    for each_recipe in recipe_output.keys():
+        line = recipe_output[each_recipe]
         writer.writerow(line)
+
+with open(ingredients_file, "w") as output:
+    writer = csv.writer(output, delimiter = delimiter,lineterminator='\n')
+    for each_line in ingredients_output:
+        writer.writerow(each_line)
 
