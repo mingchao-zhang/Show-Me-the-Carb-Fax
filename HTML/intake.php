5430a8bbf8da33abc81554b07bdb27d0e9516cb4
stevegonzo <stevejgonzo@gmail.com>
2019-04-05 16:53:55 -0500

    updated intake

--- HTML/intake.php
+++ HTML/intake.php
@@ -221,7 +221,8 @@
                     <p>
                       <a href="<?php echo $_SERVER['PHP_SELF']; ?>">Back to top</a>
                     </p>
-                  </footer>  
+                  </footer>
+        </div> 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
