7bee5801bb204a0b4dca00fff1766d5753b1b091
stevegonzo <stevejgonzo@gmail.com>
2019-04-05 15:31:51 -0500

    Updated profile

--- HTML/profile.php
+++ HTML/profile.php
@@ -114,7 +114,7 @@
                         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                           <div class="col p-4 d-flex flex-column position-static">
                             <strong class="d-inline-block mb-2 text-primary">Your Nutrient Targets</strong>
-                            <h3 class="mb-0">See your current nutrient targets!</h3>
+                            <h3 class="mb-0">View your current nutrient targets!</h3>
                             <div class="mb-1 text-muted">Macro and Micro Nutrients</div>
                             <p class="mb-auto">As determined by your profile and FDA recommendations.</p>
                             <a href="#" class="stretched-link">View Targets</a>
