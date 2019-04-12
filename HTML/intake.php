e9d1fd5f6ec02082d6ca1dd0e337ff304c47f589
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 18:41:48 -0500

    making progress

--- HTML/intake.php
+++ HTML/intake.php
@@ -235,10 +235,20 @@
                         <div class="jumbotron">
                           <h3 class="h3 mb-3 font-weight-normal">Your Weekly Log</h3>
                           <span class="input-group-btn">
+              
+                          <div class="input-group">
+                <span class="input-group-btn">
+              <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+                  <span class="glyphicon glyphicon-minus"></span>
+                </button>
+                </span>
+                <input type="text" name="quant[1]" class="form-control input-number" value="1" min="1" max="10">
+                <span class="input-group-btn">
               <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
                   <span class="glyphicon glyphicon-plus"></span>
                 </button>
                 </span>
+            </div>
                           <!-- food item table start -->
                           <!--
                           <div id="weekly_log_container">
