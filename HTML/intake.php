4a12d748dd6daba3a89d5d75458081f18845c2d3
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 18:50:39 -0500

    making progress

--- HTML/intake.php
+++ HTML/intake.php
@@ -83,7 +83,9 @@
     <link rel="stylesheet" href="blog.css">
     <!-- weekly_log css -->  
     <!-- Latest compiled and minified CSS -->
+    <!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    -->
     <title>Nutrient Intake</title>
     <style>
        .bd-placeholder-img {
@@ -292,7 +294,11 @@
                                   $row['quantity'] . "</td>";
                                   echo "</tr>";
                                 }
-
+                                /*
+                                <button type="button" class="btn btn-default btn-lg">
+  <span class="glyphicon glyphicon-star" aria-hidden="true"></span> Star
+</button>
+*/
                                 // Close Database Connection
                                 mysql_free_result($ateResult);
                                 mysql_close($dbconnect);
