97b5c6c11704835834253e13e17bb1fca352c846
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 19:22:09 -0500

    making progress

--- HTML/weekly_log.js
+++ HTML/weekly_log.js
@@ -1,7 +1,8 @@
 function update_quantity(add) {
+    console.log("hello???")
     if (add) {
         console.log("increase the quantity by one");
     }
 }
 
-$(".weekly_log_plus_button").on( "click", update_quantity );
+$(".weekly_log_plus_button").on( "click", update_quantity(true) );
