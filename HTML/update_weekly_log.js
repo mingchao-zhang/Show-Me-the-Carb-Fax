f70a83580801236385c7f4ae30634f04663aa59b
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 23:14:36 -0500

    making progress

--- HTML/update_weekly_log.js
+++ HTML/update_weekly_log.js
@@ -1,11 +1,22 @@
 function update_quantity(add, id) {
     console.log(id)
     if (add) {
-        console.log("increase the quantity by one???");
+        console.log("increase the quantity by one???")
+        id += "&increase=true"
     }
     else {
-        console.log("decrease the quantity by one");
+        console.log("decrease the quantity by one")
+        id += "&increase=false"
     }
+    console.log(id)
+    $.ajax({
+        cache: false,
+        url: "update_weekly_log.php",
+        data: id,
+        success: function(data) {
+            $("#weekly_log_content").html(data)
+        }
+    })
 }
 
 $(".weekly_log_plus_button").on( "click", function(event) {
