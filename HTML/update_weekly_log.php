aea4ea9e5f0aaf06f0dea35f37d6d7216d3637a6
Mingchao Zhang <mz5@illinois.edu>
2019-04-13 00:50:28 -0500

    making progress

--- HTML/update_weekly_log.php
+++ HTML/update_weekly_log.php
@@ -20,7 +20,7 @@
     $update_query = "UPDATE ate 
                      SET quantity = quantity + $add, date = date
                      WHERE username = '$username' AND 
-                           CAST(date AS varchar(10)) = '$date' AND 
+                           CAST(date AS DATE) = '$date' AND 
                            foodID = '$foodID'
                     ";
     $update_result = mysql_query($update_query, $dbconnect);
