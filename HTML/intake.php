799ced139b321c2860d0b38c79bdb6c7bba5a9dc
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 19:23:05 -0500

    making progress

--- HTML/intake.php
+++ HTML/intake.php
@@ -297,5 +297,6 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="food_search.js"></script>
         <!-- live food search -->
+        <script src="weekly_log.js"></script>
     </body>
 </html>
