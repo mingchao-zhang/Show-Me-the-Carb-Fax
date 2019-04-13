c12e52afe1a353c2ce1fa8a0f753c3b1f7b80fbe
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 23:39:17 -0500

    making progress

--- HTML/update_weekly_log.js
+++ HTML/update_weekly_log.js
@@ -4,16 +4,15 @@ function update_quantity(add, id_and_date) {
     console.log(params)
     var jquery_data_str = "id=" + params[0] + "&date=" + params[1] + "&add=" + params[2]
     console.log(jquery_data_str)
-    /*
+    
     $.ajax({
         cache: false,
         url: "update_weekly_log.php",
-        data: id,
+        data: jquery_data_str,
         success: function(data) {
             $("#weekly_log_content").html(data)
         }
     })
-    */
 }
 
 $(".weekly_log_plus_button").on( "click", function(event) {
