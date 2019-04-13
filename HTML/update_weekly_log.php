a26ebce50543df4b51082f9ffc801d914f410a0a
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 23:38:26 -0500

    making progress

---
+++ HTML/update_weekly_log.php
@@ -0,0 +1,25 @@
+<?php
+    // Database Connection
+    $dbconnect = mysql_connect("localhost", "root", "carbfax411");
+    if ( !$dbconnect ) {
+        die('Cannot connect: ' . mysql_error());
+    }
+
+    $db_selected = mysql_select_db("411_project_db", $dbconnect);
+    if ( !$db_selected ) {
+        die('Cant use database: ' . mysql_error());
+    }
+
+    // update quantity
+    $id = $_GET['id'];
+    $date = $_GET['date'];
+    $add = $_GET['add'];
+    echo $id . $date . $add;
+
+    include "weekly_log.php";
+
+
+    // Close Database Connection
+    mysql_free_result($ateResult);
+    mysql_close($dbconnect);
+?>
