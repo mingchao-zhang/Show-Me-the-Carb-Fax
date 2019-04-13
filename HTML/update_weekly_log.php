9e27b4922a5211a671a6ae61eb6880ab6dc9aecf
Mingchao Zhang <mz5@illinois.edu>
2019-04-12 23:53:02 -0500

    making progress

--- HTML/update_weekly_log.php
+++ HTML/update_weekly_log.php
@@ -26,4 +26,42 @@
     mysql_free_result($ateResult);
     mysql_close($dbconnect);
     */
+    // Database Connection
+    $dbconnect = mysql_connect("localhost", "root", "carbfax411");
+    if ( !$dbconnect ) {
+        die('Cannot connect: ' . mysql_error());
+    }
+
+    $db_selected = mysql_select_db("411_project_db", $dbconnect);
+    if ( !$db_selected ) {
+        die('Cant use database: ' . mysql_error());
+    }
+
+    // Query to Get Eaten Items
+    $queryAte = "SELECT products.name AS name, ate.foodID AS ID, ate.date AS date, ate.quantity AS quantity FROM ate, products WHERE username = '$username' and ate.foodID = products.foodID";
+    $ateResult = mysql_query($queryAte, $dbconnect);
+
+    if ( !$ateResult ) {
+        die('Invalid Query: ' . mysql_error());
+    }
+    while ( $row = mysql_fetch_assoc($ateResult) ) {
+        $food_id = $row['ID'];
+        $date = $row['date'];
+        $row_id = $food_id . "&" . $date;
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['ID'] . "</td>";
+        echo "<td>" . $row['date'] . "</td>";
+        echo "<td>" . 
+        "<button name='remove' class='btn btn-sm btn-primary btn-block weekly_log_plus_button' type='submit' id=$row_id>+</button>"
+        . $row['quantity'] 
+        . "<button name='remove' class='btn btn-sm btn-primary btn-block weekly_log_minus_button' type='submit' id=$row_id>-</button>"
+        . "</td>";
+        echo "</tr>";
+        echo "in while loop";
+    }
+    echo "executed in weekly_log.php";
+    // Close Database Connection
+    mysql_free_result($ateResult);
+    mysql_close($dbconnect);
 ?>
