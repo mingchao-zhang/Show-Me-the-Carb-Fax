5530b5a40d933ed6e27160e670a7f3594d6c80f5
stevegonzo <stevejgonzo@gmail.com>
2019-04-18 00:18:37 -0500

    added link back to profile

--- HTML/intake.php
+++ HTML/intake.php
@@ -121,7 +121,7 @@
                 <header class="blog-header py-3">
                     <div class="row flex-nowrap justify-content-between align-items-center">
                         <div class="col-4 pt-1">
-                          <span class="text-primary">Hello, <?php echo $name;?>!</span>
+                          <a href="profile.php" class="text-primary">Hello, <?php echo $name;?>!</a>
                         </div>
                         <div class="col-md-4 text-center">
                           <a class="blog-header-logo text-dark" href="index.html">Show Me the Carb Fax</a>
