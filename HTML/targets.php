f7f7156f462f7752b25bad97bc235ca1dc4455f1
stevegonzo <stevejgonzo@gmail.com>
2019-04-18 00:15:58 -0500

    updated targets.php

--- HTML/targets.php
+++ HTML/targets.php
@@ -25,7 +25,7 @@
     $age = $row['age'];
     $height_cms = $row['height'] * 2.54;
     $weight_kgs = $row['weight'] * 0.453592;
-    $cals_recmnd = (10 * $weight_kgs) + (6.25 * $height_cms) - (5 * $age) + 5;
+    $cals_recmnd = ceil((10 * $weight_kgs) + (6.25 * $height_cms) - (5 * $age) + 5);
     $carbs_recmnd = ceil($cals_recmnd * 0.5 / 4);
     $prot_recmnd = ceil($cals_recmnd * 0.25 / 4);
     $fat_recmnd = ceil($cals_recmnd * 0.25 / 9);
