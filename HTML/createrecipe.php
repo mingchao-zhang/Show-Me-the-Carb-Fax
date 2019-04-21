96ed3cbd7a645036f2f2efee9c4607d6bc1cf490
Mingchao Zhang <mz5@illinois.edu>
2019-04-20 21:10:25 -0500

    making progress

--- HTML/createrecipe.php
+++ HTML/createrecipe.php
@@ -40,6 +40,8 @@
     <!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     -->
+    <!-- recipe creation form CSS -->
+    <link rel="stylesheet" href="create_recipe.css">
     <title>Create a Recipe</title>
     <style>
        .bd-placeholder-img {
