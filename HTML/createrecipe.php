7817af79485eb0b797dfb08c6b32ba38c164a4dc
Mingchao Zhang <mz5@illinois.edu>
2019-04-20 21:17:38 -0500

    making progress

--- HTML/createrecipe.php
+++ HTML/createrecipe.php
@@ -100,10 +100,13 @@
                       <div id="recipe_form_container">
                         <div id="recipe_info_box">
                       <!--Live search start-->
-                      <h3 class="h3 mb-3 font-weight-normal">Search Item IDs</h3>
-                      <input type="radio" name="search_option" id="_product" value="product" checked="checked"/> Product<br>    
-                      <input type="radio" name="search_option" id="_recipe" value="recipe" /> Recipe<br>
-                      <input type="text" id="food_search" placeholder="Enter Item Name">
+                      <div class="flex_dummy">
+                        <input type="radio" name="search_option" id="_product" value="product" checked="checked"/> Product<br>    
+                        <input type="radio" name="search_option" id="_recipe" value="recipe" /> Recipe<br>
+                      </div>
+                      <div class="flex_dummy">
+                        <input type="text" id="food_search" placeholder="Enter Item Name">
+                      </div>
                       <div id="display_item_container">
                         <div id="food_suggestion"></div>
                       </div>            
