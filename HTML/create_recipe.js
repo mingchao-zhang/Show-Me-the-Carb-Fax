4ab8df865e70f7c1c7cf575cc776d9ae3e0a1de9
Mingchao Zhang <mz5@illinois.edu>
2019-04-21 22:36:33 -0500

    making progress

--- HTML/create_recipe.js
+++ HTML/create_recipe.js
@@ -28,7 +28,7 @@ $(document).on("click", ".food_search_item", function(event) {
         div_elem = event.target.id
     }
     console.log("div_elem: ", div_elem)
-    param_arr = div_elem.split(" ")
+    param_arr = div_elem.split("*")
     console.log(param_arr)
     item_name = param_arr[0]
     item_id = param_arr[1]
