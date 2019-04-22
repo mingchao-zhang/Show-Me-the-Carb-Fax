9768651a841698dc6b1000419a71dae6f3729169
Mingchao Zhang <mz5@illinois.edu>
2019-04-22 00:28:27 -0500

    making progress

--- HTML/create_recipe.js
+++ HTML/create_recipe.js
@@ -35,5 +35,11 @@ $(document).on("click", ".food_search_item", function(event) {
 var item_arr = []
 // Add the item selected to the right display area
 $(document).on("click", '#_add_item_button', function(event) {
-    console.log($("#item_selected_text").html())
+    var item = []
+    var name_and_id = $("#item_selected_text").html();
+    name_and_id = name_and_id.split(";")
+    item.push(name_and_id[0])
+    item.push(name_and_id[1].slice(1)
+    item_arr.push(item)
+    console.log(item_arr)
 })
