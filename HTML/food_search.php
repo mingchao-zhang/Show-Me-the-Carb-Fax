ce4434f215cb0eacc2ea2b09f8db363b54f34a58
Mingchao Zhang <mz5@illinois.edu>
2019-04-21 21:40:35 -0500

    making progress

--- HTML/food_search.php
+++ HTML/food_search.php
@@ -43,7 +43,7 @@
         while ($row = mysql_fetch_assoc($searchResults)) {
             $name_and_id = $row['name'] . "&" . $row['foodId'];
             echo $name_and_id;
-            echo "<div class='food_search_item' id=$name_and_id><div class='flex_dummy'><p>" . $row['name'] . ", " . $row['foodId'] . "</p></div></div>";
+            echo "<div class='food_search_item' id=" . $name_and_id . "><div class='flex_dummy'><p>" . $row['name'] . ", " . $row['foodId'] . "</p></div></div>";
         }
     }
 ?>
