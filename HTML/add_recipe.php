353e5bcd6082d08d40880181d537dd7ad6e72c96
Mingchao Zhang <mz5@illinois.edu>
2019-04-30 01:46:21 -0500

    making progress

--- HTML/add_recipe.php
+++ HTML/add_recipe.php
@@ -11,6 +11,34 @@
         if (!$db_selected) {
           die('Cant use database: ' . mysql_error());
         }
+
+        //Get variables
+        $recipe_name = $_GET['recipe_name'];
+        $recipe_description = $_GET["recipe_description"];
+        $item_name = $_GET["item_name"];
+        $item_id = $_GET["item_id"];
+        $quantity_unit = $_GET["quantity_unit"];
+        $quantity = $_GET["quantity"];
+
+        // Do queries
+        $get_recipe_id_query = "(SELECT MAX(foodID) FROM recipes) + 1";
+        $result = mysql_query($get_recipe_id_query, $dbconnect);
+        if (!$result) {
+            die("Invalid Query: " . mysql_error());
+        }
+        $new_recipe_id = $result;
+        echo $new_recipe_id;
+/*
+        $recipe_insert_query = "INSERT INTO recipes (`foodID`,`name`,`calories`, `total_carbs`, `sugar`, `protein`, `total_fat`, `sodium`, `cholesterol`, `directions`) 
+        VALUES ($new_recipe_id, $recipe_name, 0, 0, 0, 0, 0, 0, 0, $recipe_description);"
+        $result = mysql_query($recipe_insert_query, $dbconnect);
+        if (!$result) {
+            die("Invalid Query: " . mysql_error());
+        }
+
+        $contains_insert_query = "INSERT INTO contains (`recipe_foodID`,`product_foodID`,`quantity`, `measurement_std`, `volume`, `weight`) 
+        VALUES ($new_recipe_id, $item_id, $quantity, "", -1, -1);"
+*/
         echo $_GET['recipe_name'] . " " . $_GET["recipe_description"] . " " . $_GET["item_name"] . " " . $_GET["item_id"] . " " . $_GET["quantity_unit"] . " " . $_GET["quantity"];
     }
 ?>
--- HTML/create_recipe.js
+++ HTML/create_recipe.js
@@ -146,22 +146,3 @@ $(document).on("click", "#submit_recipe_btn", function(event) {
         item_arr = []
     }
 })
-
-/*
-function search_food() {
-    var search_option = "product";
-    if ( document.getElementById('_recipe').checked ) {
-        search_option = "recipe";               
-    }
-    var search_name = $("#food_search").val();
-    $.ajax({
-        cache: false,
-        url: "food_search.php",
-        data: "name=" + search_name + "&option=" + search_option,
-        success: function(data) {
-            $("#food_suggestion").html(data);
-        }
-    })
-}   
-*/
-    
