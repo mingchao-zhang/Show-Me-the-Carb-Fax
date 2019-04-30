760923ff5e3fdd5a91071df78ae14aebc7662b44
Mingchao Zhang <mz5@illinois.edu>
2019-04-30 15:22:47 -0500

    making progress

--- HTML/get_new_recipe_id.php
+++ HTML/get_new_recipe_id.php
@@ -20,6 +20,7 @@
         $new_recipe_id = mysql_fetch_array($result)[0] + 1;
 
         # echo the recipe id for insertions into contains table
+        echo $result;
         echo $new_recipe_id;
 
         mysql_free_result($result);
