66f4272d3e8a2b17539140f072efc567f49bee68
Rohan Prabhakar <36459840+rohan-bp@users.noreply.github.com>
2019-04-29 20:54:43 -0500

    Testing Recommendaton

--- Workspace/Recommender/recommendations.py
+++ Workspace/Recommender/recommendations.py
@@ -60,58 +60,58 @@ try:
         if(c):
            daily_cholesterol += float(c)
      
-"""
-    # Retrieve nutrient information from the products table
-    query = ("SELECT SUM(P.calories * A.quantity),SUM(P.total_carbs * A.quantity),SUM(P.sugars * A.quantity)"
-     ",SUM(P.protein * A.quantity),SUM(P.total_fat * A.quantity),SUM(P.sodium * A.quantity),SUM(P.cholesterol * A.quantity)"
-     "FROM products AS P, ate as A WHERE P.foodID = A.foodID AND A.username = %s;")
-    cursor.execute(query,(user_id,))
-    
-    daily_calories += float(cursor[0])
-    daily_carbs += float(cursor[1])
-    daily_sugar += float(cursor[2])
-    daily_protein += float(cursor[3])
-    daily_fat += float(cursor[4])
-    daily_sodium += float(cursor[5])
-    daily_cholesterol += float(cursor[7])
-    
-    # Retrieve the list of unique dates to compute a per-day average
-    query = ("SELECT COUNT(DISTINCT date) FROM ate WHERE username = %s;")
-    cursor.execute(query,(user_id,))
 
-    num_days = int(cursor[0])
-    
-    daily_calories /= num_days
-    daily_carbs /= num_days
-    daily_sugar /= num_days
-    daily_protein /= num_days
-    daily_fat /= num_days
-    daily_sodium /= num_days
-    daily_cholesterol /= num_days
+#    # Retrieve nutrient information from the products table
+#    query = ("SELECT SUM(P.calories * A.quantity),SUM(P.total_carbs * A.quantity),SUM(P.sugars * A.quantity)"
+#     ",SUM(P.protein * A.quantity),SUM(P.total_fat * A.quantity),SUM(P.sodium * A.quantity),SUM(P.cholesterol * A.quantity)"
+#     "FROM products AS P, ate as A WHERE P.foodID = A.foodID AND A.username = %s;")
+#    cursor.execute(query,(user_id,))
+#
+#    daily_calories += float(cursor[0])
+#    daily_carbs += float(cursor[1])
+#    daily_sugar += float(cursor[2])
+#    daily_protein += float(cursor[3])
+#    daily_fat += float(cursor[4])
+#    daily_sodium += float(cursor[5])
+#    daily_cholesterol += float(cursor[7])
+#
+#    # Retrieve the list of unique dates to compute a per-day average
+#    query = ("SELECT COUNT(DISTINCT date) FROM ate WHERE username = %s;")
+#    cursor.execute(query,(user_id,))
+#
+#    num_days = int(cursor[0])
+#
+#    daily_calories /= num_days
+#    daily_carbs /= num_days
+#    daily_sugar /= num_days
+#    daily_protein /= num_days
+#    daily_fat /= num_days
+#    daily_sodium /= num_days
+#    daily_cholesterol /= num_days
+#
+#    # Retrieve the targets if applicable
+#    query = ("SELECT calorie_target,carb_target,fat_target,protein_target FROM users"
+#    "WHERE username = %s;")
+#    cursor.execute(query,(user_id,))
+#
+#
+#
+#    # Retrieve a list of potential recipes i.e. recipes that the user has not consumed
+#    query = ("SELECT foodID,calories,total_carbs,sugar,protein,total_fat,sodium,cholestrol"
+#    "FROM recipes WHERE recipes.foodID NOT IN"
+#    "(SELECT foodID from ate WHERE username = %s);")
+#    cursor.execute(query,(user_id,))
+#
+#    recipes = cursor[:]
+#
+#    print(daily_calories)
+#    print(daily_carbs)
+#    print(daily_sugar)
+#    print(daily_protein)
+#    print(daily_fat)
+#    print(daily_cholesterol)
+#    print("Done!")
 
-    # Retrieve the targets if applicable
-    query = ("SELECT calorie_target,carb_target,fat_target,protein_target FROM users"
-    "WHERE username = %s;")
-    cursor.execute(query,(user_id,))
-    
-    
-     
-    # Retrieve a list of potential recipes i.e. recipes that the user has not consumed
-    query = ("SELECT foodID,calories,total_carbs,sugar,protein,total_fat,sodium,cholestrol"
-    "FROM recipes WHERE recipes.foodID NOT IN"
-    "(SELECT foodID from ate WHERE username = %s);")
-    cursor.execute(query,(user_id,))
-
-    recipes = cursor[:]
-    
-    print(daily_calories)
-    print(daily_carbs)
-    print(daily_sugar)
-    print(daily_protein)
-    print(daily_fat)
-    print(daily_cholesterol)
-    print("Done!")
-"""
     connection.close()
 
 
