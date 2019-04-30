e88cbc88e41486a5b846da36a298ae1492cf7f98
Mingchao Zhang <mz5@illinois.edu>
2019-04-30 15:21:35 -0500

    making progress

--- HTML/create_recipe.js
+++ HTML/create_recipe.js
@@ -125,7 +125,9 @@ $(document).on("click", "#submit_recipe_btn", function(event) {
         $.ajax({
             cache: false,
             url: "get_new_recipe_id.php",
-            success: function(new_recipe_id) {
+            data: "",
+            success: function(data) {
+                var new_recipe_id = data
                 console.log("new_recipe_id: ", new_recipe_id)
                 var data_str = "recipe_name=" + recipe_name + "&" + 
                                "recipe_description=" + recipe_description + "&" +
