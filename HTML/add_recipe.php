a533fc3e3d1d0123d89ddeed259dac231e700c7f
Mingchao Zhang <mz5@illinois.edu>
2019-04-30 01:12:04 -0500

    making progress

--- HTML/add_recipe.php
+++ HTML/add_recipe.php
@@ -1,11 +1,14 @@
 <?php
+    echo "line 2";
     if ( isset($_GET['name']) && !empty($_GET['name']) ) {
         // Database Connection
+        echo "line 5";
         $dbconnect = mysql_connect("localhost", "root", "carbfax411");
         if(!$dbconnect){
             die('Cannot connect: ' . mysql_error());
         }
 
+        echo "line 11";
         $db_selected = mysql_select_db("411_project_db", $dbconnect);
 
         if (!$db_selected) {
