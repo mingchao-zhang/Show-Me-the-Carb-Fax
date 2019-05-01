865a5d575483ab19106fca4d2f17b6d75e88f508
Rohan Prabhakar <36459840+rohan-bp@users.noreply.github.com>
2019-05-01 16:23:08 -0500

    Adding new recommender

--- Workspace/Recommender/recommendations.py
+++ Workspace/Recommender/recommendations.py
@@ -60,136 +60,136 @@ try:
            daily_cholesterol += float(c)
      
 
-    # Retrieve nutrient information from the products table
-    query = ("SELECT SUM(P.calories * A.quantity),SUM(P.total_carbs * A.quantity),SUM(P.sugars * A.quantity) "
-     ",SUM(P.protein * A.quantity),SUM(P.total_fat * A.quantity),SUM(P.sodium * A.quantity),SUM(P.cholesterol * A.quantity) "
-     "FROM products AS P, ate as A WHERE P.foodID = A.foodID AND A.username = %s;")
-    cursor.execute(query,(user_id,))
-
-    for (cal,carbs,s,p,f,sod,c) in cursor:
-
-        if(cal):
-            daily_calories += float(cal)
-        if(carbs):
-            daily_carbs += float(carbs)
-        if(s):
-            daily_sugar += float(s)
-        if(p):
-            daily_protein += float(p)
-        if(f):
-            daily_fat += float(f)
-        if(sod):
-            daily_sodium += float(sod)
-        if(c):
-            daily_cholesterol += float(c)
-
-    # Retrieve the list of unique dates to compute a per-day average
-    query = ("SELECT COUNT(DISTINCT (EXTRACT(DAY FROM date))) FROM ate WHERE username = %s;")
-    cursor.execute(query,(user_id,))
-    
-    for (days,) in cursor:
-        num_days = int(days)
-
-    daily_calories /= num_days
-    daily_carbs /= num_days
-    daily_sugar /= num_days
-    daily_protein /= num_days
-    daily_fat /= num_days
-    daily_sodium /= num_days
-    daily_cholesterol /= num_days
-
-#    # Retrieve the targets if applicable
-    query = ("SELECT calorie_target,carb_target,fat_target,protein_target FROM users "
-    "WHERE username = %s;")
-    cursor.execute(query,(user_id,))
-
-    cal_target = 0
-    carb_target = 0
-    fat_target = 0
-    prot_target = 0
-
-    for  (c1,c2,f,p) in cursor:
-
-        if(c1):
-            cal_target = cal
-        if(c2):
-            carb_target = carbs
-        if(f):
-            fat_target = f
-        if(p):
-            prot_target = p
-
-    query = "SELECT age,height,weight WHERE username = %s;"
-    cursor.execute(query,(user_id,))
-
-    a = 0
-    h = 0
-    w = 0
-    for (a,h,w) in cursor:
-        age = float(a)
-        height = float(h)*2.54
-        weight = float(w)*0.453592;
-
-
-    cals_recmnd = ceil((10 * weight) + (6.25 *height) - (5 * age) + 5);
-    carbs_recmnd = ceil(cals_recmnd * 0.5 / 4);
-    prot_recmnd = ceil(cals_recmnd * 0.25 / 4);
-    fat_recmnd = ceil(cals_recmnd * 0.25 / 9);
-
-    if(carb_target == 0):
-
-        carb_target = carbs_recmnd
-
-    if(fat_target == 0):
-
-        fat_target = fat_recmnd
-
-    if(prot_target == 0):
-
-        prot_target = prot_recmnd
-
-    # Based on average estimate
-    sod_target = 2500
-    cholesterol_target = 250
-
-
-    # Retrieve a list of potential recipes i.e. recipes that the user has not consumed
-    query = ("SELECT foodID,name,calories,total_carbs,sugar,protein,total_fat,sodium,cholesterol "
-    "FROM recipes WHERE recipes.foodID NOT IN "
-    "(SELECT foodID from ate WHERE username = %s);")
-    cursor.execute(query,(user_id,))
-    recipes = []
-
-    for (id,name,c,tc,s,p,f,sod,ch) in cursor:
-        recipes.append([id,name,float(c),float(tc),float(s),float(p),float(f),float(sod),float(ch)])
-
-
-    # We recommend those recipes that minimize the mean-squared distance between the various nutrient values and recommendations
-    # We assume that the standard portion is about 500 calories and multiply all the values o
-
-    recommendations = []
-    for recipe in recipes:
-        
-        factor = 500/recipe[2] #Factor to standardize all recipes
-
-        diff = (0.15*(fat_target/4 - recipe[6]*factor)**2 + 0.15*(prot_target/4 - recipe[5]*factor)**2 + 0.15*(sod_target/4 - recipe[7]*factor)**2 + 0.15*(cholesterol_target/4 - recipe[8]*factor)**2 + 0.15(carb_target/4 - recipe[3]*factor)**2)
-
-        recommendations.append(diff,recipe[0],recipe[1])
-
-    recommendations.sort(key=lambda tup: tup[0])
-
-    for i in range(10):
-        print(recommendations[i][2])
-
-    print(daily_calories)
-    print(daily_carbs)
-    print(daily_sugar)
-    print(daily_protein)
-    print(daily_fat)
-    print(daily_cholesterol)
-    print("Done!")
-
-    connection.close()
+#    # Retrieve nutrient information from the products table
+#    query = ("SELECT SUM(P.calories * A.quantity),SUM(P.total_carbs * A.quantity),SUM(P.sugars * A.quantity) "
+#     ",SUM(P.protein * A.quantity),SUM(P.total_fat * A.quantity),SUM(P.sodium * A.quantity),SUM(P.cholesterol * A.quantity) "
+#     "FROM products AS P, ate as A WHERE P.foodID = A.foodID AND A.username = %s;")
+#    cursor.execute(query,(user_id,))
+#
+#    for (cal,carbs,s,p,f,sod,c) in cursor:
+#
+#        if(cal):
+#            daily_calories += float(cal)
+#        if(carbs):
+#            daily_carbs += float(carbs)
+#        if(s):
+#            daily_sugar += float(s)
+#        if(p):
+#            daily_protein += float(p)
+#        if(f):
+#            daily_fat += float(f)
+#        if(sod):
+#            daily_sodium += float(sod)
+#        if(c):
+#            daily_cholesterol += float(c)
+#
+#    # Retrieve the list of unique dates to compute a per-day average
+#    query = ("SELECT COUNT(DISTINCT (EXTRACT(DAY FROM date))) FROM ate WHERE username = %s;")
+#    cursor.execute(query,(user_id,))
+#    
+#    for (days,) in cursor:
+#        num_days = int(days)
+#
+#    daily_calories /= num_days
+#    daily_carbs /= num_days
+#    daily_sugar /= num_days
+#    daily_protein /= num_days
+#    daily_fat /= num_days
+#    daily_sodium /= num_days
+#    daily_cholesterol /= num_days
+#
+##    # Retrieve the targets if applicable
+#    query = ("SELECT calorie_target,carb_target,fat_target,protein_target FROM users "
+#    "WHERE username = %s;")
+#    cursor.execute(query,(user_id,))
+#
+#    cal_target = 0
+#    carb_target = 0
+#    fat_target = 0
+#    prot_target = 0
+#
+#    for  (c1,c2,f,p) in cursor:
+#
+#        if(c1):
+#            cal_target = cal
+#        if(c2):
+#            carb_target = carbs
+#        if(f):
+#            fat_target = f
+#        if(p):
+#            prot_target = p
+#
+#    query = "SELECT age,height,weight WHERE username = %s;"
+#    cursor.execute(query,(user_id,))
+#
+#    a = 0
+#    h = 0
+#    w = 0
+#    for (a,h,w) in cursor:
+#        age = float(a)
+#        height = float(h)*2.54
+#        weight = float(w)*0.453592;
+#
+#
+#    cals_recmnd = ceil((10 * weight) + (6.25 *height) - (5 * age) + 5);
+#    carbs_recmnd = ceil(cals_recmnd * 0.5 / 4);
+#    prot_recmnd = ceil(cals_recmnd * 0.25 / 4);
+#    fat_recmnd = ceil(cals_recmnd * 0.25 / 9);
+#
+#    if(carb_target == 0):
+#
+#        carb_target = carbs_recmnd
+#
+#    if(fat_target == 0):
+#
+#        fat_target = fat_recmnd
+#
+#    if(prot_target == 0):
+#
+#        prot_target = prot_recmnd
+#
+#    # Based on average estimate
+#    sod_target = 2500
+#    cholesterol_target = 250
+#
+#
+#    # Retrieve a list of potential recipes i.e. recipes that the user has not consumed
+#    query = ("SELECT foodID,name,calories,total_carbs,sugar,protein,total_fat,sodium,cholesterol "
+#    "FROM recipes WHERE recipes.foodID NOT IN "
+#    "(SELECT foodID from ate WHERE username = %s);")
+#    cursor.execute(query,(user_id,))
+#    recipes = []
+#
+#    for (id,name,c,tc,s,p,f,sod,ch) in cursor:
+#        recipes.append([id,name,float(c),float(tc),float(s),float(p),float(f),float(sod),float(ch)])
+#
+#
+#    # We recommend those recipes that minimize the mean-squared distance between the various nutrient values and recommendations
+#    # We assume that the standard portion is about 500 calories and multiply all the values o
+#
+#    recommendations = []
+#    for recipe in recipes:
+#        
+#        factor = 500/recipe[2] #Factor to standardize all recipes
+#
+#        diff = (0.15*(fat_target/4 - recipe[6]*factor)**2 + 0.15*(prot_target/4 - recipe[5]*factor)**2 + 0.15*(sod_target/4 - recipe[7]*factor)**2 + 0.15*(cholesterol_target/4 - recipe[8]*factor)**2 + 0.15(carb_target/4 - recipe[3]*factor)**2)
+#
+#        recommendations.append(diff,recipe[0],recipe[1])
+#
+#    recommendations.sort(key=lambda tup: tup[0])
+#
+#    for i in range(10):
+#        print(recommendations[i][2])
+#
+#    print(daily_calories)
+#    print(daily_carbs)
+#    print(daily_sugar)
+#    print(daily_protein)
+#    print(daily_fat)
+#    print(daily_cholesterol)
+#    print("Done!")
+#
+#    connection.close()
 
 
 except mysql.Error as e:
