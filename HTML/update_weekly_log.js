f04e60d7b50b9b7fba2d40b07cf8dda769c15834
Smit Desai <smitdesai@sbcglobal.net>
2019-05-02 03:30:14 -0500

    Updating update_weekly_log.php to include transaction

--- HTML/update_weekly_log.js
+++ HTML/update_weekly_log.js
@@ -1,21 +1,22 @@
 function update_quantity(add, id_and_date) {
     var params = id_and_date.split("&")
     params.push(add.toString())
-    var jquery_data_str = "username=" + params[0] + 
-                          "&id=" + params[1] + 
-                          "&date=" + params[2] + 
+    var jquery_data_str = "username=" + params[0] +
+                          "&id=" + params[1] +
+                          "&date=" + params[2] +
                           "&add=" + params[3]
-    
+
     $.ajax({
         cache: false,
         url: "update_weekly_log.php",
         data: jquery_data_str,
         success: function(data) {
+            console.log(data)
             $("#weekly_log_content").html(data)
         }
     })
 
-    
+
 }
 
 // DO NOT USE $(".weekly_log_plus_button").on( "click", '.option', function(event)
@@ -29,4 +30,3 @@ $(document).on('click', ".weekly_log_minus_button",   function(event) {
     update_quantity(-1, event.target.id)
  }
 );
-
