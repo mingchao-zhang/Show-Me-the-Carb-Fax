1ace8613e9aaaf79bc0484dbf9a9256ace121566
Steve Gonzalez <stevejgonzo@gmail.com>
2019-05-02 03:08:13 -0500

    testing recipe display

--- HTML/recipes.php
+++ HTML/recipes.php
@@ -5,7 +5,6 @@
     $command = escapeshellcmd("python recommendations.py '$username' ");
     $output = shell_exec($command);
     $recipe_IDS = explode(",", $output);
-    echo $recipe_IDS;
 ?>
 <!doctype html>
 <html lang="en">
