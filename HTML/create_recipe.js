98f73f36471d20a1a9d78f17122385ae5f985919
Mingchao Zhang <mz5@illinois.edu>
2019-05-01 23:50:16 -0500

    making progress

--- HTML/create_recipe.js
+++ HTML/create_recipe.js
@@ -151,7 +151,9 @@ $(document).on("click", "#submit_recipe_btn", function(event) {
                                 url: "update_contains.php",
                                 data: _data,
                                 success: function(data) {
+                                    
                                     done_count += 1
+                                    console.log(_data, done_count)
                                     if (done_count === item_arr.length) {
                                         console.log("async????????????????")
                                         $("#recipe_added_msg").html("Recipe Added")
