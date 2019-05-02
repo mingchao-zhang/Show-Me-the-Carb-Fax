2553a7b7b609662339ca51bdb182749a8f467fcd
Smit Desai <smitdesai@sbcglobal.net>
2019-05-02 03:11:30 -0500

    Updating update_weekly_log.php to include transaction

--- HTML/update_weekly_log.php
+++ HTML/update_weekly_log.php
@@ -17,28 +17,28 @@
     $add = intval($_GET['add']);
     // update quantity and delete items with quantity 0
     // date: yyyy-mm-dd
- echo "TEST1";
+ echo "<h4>TEST1</h4>";
     try {
-        echo "TEST2";
+        echo "<h4>TEST2</h4>";
         $this->pdo->beginTransaction();
-        echo "TEST3";
+        echo "<h4>TEST3</h4>";
         $update_query = "UPDATE ate
                             SET quantity = quantity + $add, date = date
                             WHERE username = '$username' AND
                             DATEDIFF(date, '$date') = 0 AND
                             foodID = '$foodID'";
-echo "TEST4";
+echo "<h4>TEST4</h4>";
         $delete_query = "DELETE FROM ate
                             WHERE username = '$username' AND
                             DATEDIFF(date, '$date') = 0 AND
                             foodID = '$foodID'
                             AND quantity = 0";
-                            echo "TEST5";
+                            echo "<h4>TEST5</h4>";
         $this->pdo->commit();
-        echo "TEST6";
+        echo "<h4>TEST6</h4>";
     } catch(PDOException $e) {
         $this->pdo->rollback();
-        echo "TEST7";
+        echo "<h4>TEST7</h4>";
         die($e->getMessage());
     }
 
