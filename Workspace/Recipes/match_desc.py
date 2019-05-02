48dca708898cffa29fcbc5fd01f91905f5a46a50
Rohan Prabhakar <36459840+rohan-bp@users.noreply.github.com>
2019-05-01 23:51:45 -0500

    Changes to recipe file

--- Workspace/Recipes/match_desc.py
+++ Workspace/Recipes/match_desc.py
@@ -9,7 +9,7 @@ import os
 """
 
 dataset_file = "epicurious-recipes-with-rating-and-nutrition/full_format_recipes.json"
-recipes_file = "Data/recipes.csv" # recip_id,name,description,directions
+recipes_file = "Data/formatted_recipes.csv" # recip_id,name,description,directions
 output_file = "Data/descriptions.csv"
 delimiter = "~"
 
@@ -19,7 +19,8 @@ recipes = {}
 with open(recipes_file, "r") as f:
     recipe_reader = csv.reader(f, delimiter="~")
     for recipe in recipe_reader:
-        recipes[recipe[1]] = recipe[0]
+        recipes[recipe[1]] = recipe
+        print(recipe)
 
 with open(dataset_file) as file:
     data = json.load(file)
@@ -29,9 +30,9 @@ with open(dataset_file) as file:
             recipe_title = each_recipe["title"].strip()
 
             if(each_recipe["desc"]):
-                descriptions.append([recipes[recipe_title],each_recipe["desc"]])
+                descriptions.append(recipes[recipe_title] + [each_recipe["desc"]])
             else:
-                descriptions.append([recipes[recipe_title],"Highly recommended!"])
+                descriptions.append(recipes[recipe_title] + ["Highly recommended!"])
     
         except Exception as e:
             print(e)
