b9891c7aff79f716c7ba435125e8b466ccf64c02
Steve Gonzalez <stevejgonzo@gmail.com>
2019-05-01 21:50:48 -0500

    Update log to include recipes

--- HTML/weekly_log.php
+++ HTML/weekly_log.php
@@ -33,8 +33,29 @@
         . "</td>";
         echo "</tr>";
     }
+    $queryRecipes = "SELECT recipes.name, ate.foodID, ate.date, ate.quantity FROM ate, recipes WHERE username = '$username' and ate.foodID = recipes.foodID";
+    $recipeResult = mysql_query($queryRecipes, $dbconnect);
 
+    if (!$recipeResult){
+        die("Invalid Query:" . mysql_error());
+    }
+    while ( $row = mysql_fetch_assoc($ateResult) ) {
+        $food_id = $row['ate.foodID'];
+        $date = $row['ate.date'];
+        $row_id = $username . "&" . $food_id . "&" . $date;
+        echo "<tr>";
+        echo "<td>" . $row['recipes.name'] . "</td>";
+        echo "<td>" . $row['ate.foodID'] . "</td>";
+        echo "<td>" . $row['ate.date'] . "</td>";
+        echo "<td>" . 
+        "<button name='remove' class='btn btn-sm btn-primary btn-block weekly_log_plus_button' type='submit' id=$row_id>+</button>"
+        . $row['ate.quantity'] 
+        . "<button name='remove' class='btn btn-sm btn-primary btn-block weekly_log_minus_button' type='submit' id=$row_id>-</button>"
+        . "</td>";
+        echo "</tr>";
+    }
     // Close Database Connection
     mysql_free_result($ateResult);
+    mysql_free_result($recipeResult);
     mysql_close($dbconnect);
 ?>
